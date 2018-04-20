bc7611f0e5170f3d2c7f89178b0a30a8a5e88c28
leehomlee <leehom_lzw@163.com>
2018-04-20 15:18:19 +0800

    stock

---
+++ SignalRStock/Stock.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRStock
+{
+    public class Stock
+    {
+        /// <summary>
+        /// 价格
+        /// </summary>
+        private decimal _price;
+        /// <summary>
+        /// 股票代码
+        /// </summary>
+        public string Symbol { get; set; }
+        /// <summary>
+        /// 首次设置的价格
+        /// </summary>
+        public decimal DayOpen { get; private set; }
+        public decimal Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (_price == value)
+                {
+                    return;
+                }
+
+                _price = value;
+
+                if (DayOpen == 0)
+                {
+                    DayOpen = _price;
+                }
+            }
+        }
+        public decimal Change
+        {
+            get
+            {
+                return Price - DayOpen;
+            }
+        }
+        public double PercentChange
+        {
+            get
+            {
+                return (double)Math.Round(Change / Price, 4);
+            }
+        }
+    }
+}
